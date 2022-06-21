ebab8572bacb211703ba5a8e6c4271e566654d1d
Prisana Cheng <chengprisana@gmail.com>
2022-06-21 14:22:40 -0500

    used JS to add grids instead of CSS

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Your code here
+document.createElement
+
+const squares=document.getElementsByTagName('table')[0]
+function makeRow(){
+    const row = document.createElement('tr')
+    for (let i = 0; i < 20; i++){
+        const td = document.createElement('td')
+        row.appendChild(td)
+    }
+    squares.appendChild(row)
+}
+
+makeRow()
+makeRow()
+
+
